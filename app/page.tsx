1b2a98cf143586a27cb68f3e6e3eae81b4fcaed7
hamidwise1 <hamidwise1@gmial.com>
2023-05-21 17:26:24 +0430

    remove homepage imports

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,6 @@
 'use client';
 import styles from './page.module.scss';
 import MasterLayout from './components/layout/MasterLayout';
-import { ChangeEvent, useState } from 'react';
-import CompSelect from './components/shared/form-elements/CompSelect';
 export default function Home() {
  return (
   <>
